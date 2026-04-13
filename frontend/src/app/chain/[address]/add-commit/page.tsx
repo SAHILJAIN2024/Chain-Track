80141b17e16df1bdac69ad128734a3e2dbadda08
SAHILJAIN2024 <jainsahil022@gmail.com>
2026-04-13 20:18:23 +0530

    error handling

--- frontend/src/app/chain/[address]/add-commit/page.tsx
+++ frontend/src/app/chain/[address]/add-commit/page.tsx
@@ -5,7 +5,7 @@ import { useParams } from "next/navigation";
 import { ethers } from "ethers";
 import Navbar from "../../../../components/navbar";
 import { useWallet } from "../../../../components/WalletContext";
-import ABI from "../../../../contractABI/supplyChainABI.json";
+import ABI from "../../../../contractABI/SupplyChainABI.json";
 import { motion } from "framer-motion";
 
 export default function AddCommit() {
